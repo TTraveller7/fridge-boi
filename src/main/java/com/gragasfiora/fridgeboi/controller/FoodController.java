5c655db7031b59cb3f8e51346a33c80e2d5b0a78
TTraveller7 <yinghuo777@gmail.com>
2022-07-09 20:46:47 +0800

    Refactor FoodController.java methods. Closes #9.

--- src/main/java/com/gragasfiora/fridgeboi/controller/FoodController.java
+++ src/main/java/com/gragasfiora/fridgeboi/controller/FoodController.java
@@ -1,12 +1,18 @@
 package com.gragasfiora.fridgeboi.controller;
 
-import com.gragasfiora.fridgeboi.exception.FoodNotFoundException;
+import com.gragasfiora.fridgeboi.assembler.FoodModelAssembler;
 import com.gragasfiora.fridgeboi.model.Food;
 import com.gragasfiora.fridgeboi.service.FoodService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api")
@@ -14,38 +20,44 @@ public class FoodController {
 
     private final FoodService foodService;
 
-    public FoodController(FoodService foodService) {
+    private final FoodModelAssembler foodModelAssembler;
+
+    public FoodController(FoodService foodService, FoodModelAssembler foodModelAssembler) {
         this.foodService = foodService;
+        this.foodModelAssembler = foodModelAssembler;
     }
 
     @GetMapping("/foods")
-    public List<Food> all() {
-        return foodService.findAll();
+    public CollectionModel<EntityModel<Food>> all() {
+        List<EntityModel<Food>> foodModels = foodService.findAll().stream()
+                .map(foodModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(foodModels)
+                .add(linkTo(methodOn(FoodController.class).all()).withSelfRel());
     }
 
     @GetMapping("/foods/{id}")
-    public Food one(@PathVariable String id) {
-        return foodService.findFoodById(Long.valueOf(id));
+    public EntityModel<Food> one(@PathVariable String id) {
+        Food food = foodService.findFoodById(Long.valueOf(id));
+        return foodModelAssembler.toModel(food);
     }
 
     @PostMapping("/foods")
-    public Food newFood(@RequestBody Food food) {
-        return foodService.save(food);
+    @ResponseStatus(HttpStatus.CREATED)
+    public EntityModel<Food> newFood(@RequestBody Food food) {
+        Food savedFood = foodService.save(food);
+        return foodModelAssembler.toModel(savedFood);
     }
 
     @PutMapping("/foods/{id}")
-    public Food replaceFood(@PathVariable String id, @RequestBody Food newFood) {
-        return foodService.replaceOrSave(Long.valueOf(id), newFood);
+    public EntityModel<Food> replaceFood(@PathVariable String id, @RequestBody Food newFood) {
+        Food savedFood = foodService.replaceOrSave(Long.valueOf(id), newFood);
+        return foodModelAssembler.toModel(savedFood);
     }
 
     @DeleteMapping("/foods/{id}")
     public void deleteFood(@PathVariable String id) {
         foodService.deleteById(Long.valueOf(id));
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(FoodNotFoundException.class)
-    public String handelFoodNotFound(FoodNotFoundException foodNotFoundException) {
-        return foodNotFoundException.getMessage();
     }
 }
