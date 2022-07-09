219823e0e40b30628d62034731700123473fb70f
TTraveller7 <yinghuo777@gmail.com>
2022-07-09 15:25:18 +0800

    Initialize spring boot project. Closes #1.

---
+++ src/main/java/com/gragasfiora/fridgeboi/FridgeboiApplication.java
@@ -0,0 +1,13 @@
+package com.gragasfiora.fridgeboi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeboiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeboiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gragasfiora/fridgeboi/FridgeboiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gragasfiora.fridgeboi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeboiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
