82cf1516d94d95287cf0421af9a2a4e3f815d023
Kimowarui <68160336+Kimowarui@users.noreply.github.com>
2022-07-10 03:45:57 +0800

    NA

--- app/src/components/FoodForm.js
+++ app/src/components/FoodForm.js
@@ -3,19 +3,20 @@ import React, {useState} from 'react';
 function FoodForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : '');
   const [time, setTime] = useState(props.edit ? props.edit.time : '');
-  var today = new Date();
-  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
   const handleChange = e => {
     setInput(e.target.value);
-    
+
+    let today = new Date();
+    const date=today.getDate() + "/"+ parseInt(today.getMonth()+1) +"/"+today.getFullYear();
+    setTime(date);
 
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     
-    setTime(date);
+
 
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
