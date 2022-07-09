2aec382a089dcf313afebaaa22542eaa6093e3de
TTraveller7 <yinghuo777@gmail.com>
2022-07-10 01:50:47 +0800

    Test fetching food list in App.js. Closes #13.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import FoodList from './components/FoodList';
+import { useState, useEffect } from 'react';
+import foodService from './services/FoodService';
 
 function App() {
+  const [foods, setFoods] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const getData = async () => {
+      try {
+        const response = await foodService.getAllFoods();
+        console.log(response.data._embedded.foodList);
+        setFoods(response.data._embedded.foodList);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div className="fridge-boi">
-      <FoodList />
+      <ul>
+        {foods.map(food => <li key={food.id}>{food.description}</li>)}
+      </ul>
     </div>
   );
 }
