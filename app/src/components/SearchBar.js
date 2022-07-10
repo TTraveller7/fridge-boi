b336490239ed656ddf79fd576915a9b61ab9f554
TTraveller7 <yinghuo777@gmail.com>
2022-07-10 08:21:40 +0800

    Add nav bar.

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
-import { BsSearch } from "react-icons/bs";
-import {AiOutlineCloseCircle} from "react-icons/ai"
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 function SearchBar({ data }) {
 const [filteredData, setFilteredData] = useState([]);
@@ -20,36 +23,44 @@ const handleFilter = (event) => {
   }
 };
 
-const clearInput = () => {
-  setFilteredData([]);
-  setWordEntered("");
-};
-
   return (
-    <div className='search'>
-      <div className='searchInput'>
-        <input type="text" placeholder="Enter name of the food you want ..." />
-        <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <BsSearch />
-          ) : (
-            <AiOutlineCloseCircle id="clearBtn" onClick={clearInput} />
-          )}
-        </div>
-      </div>
-      {filteredData.length != 0 && (
-        <div className="dataResult">
-          {filteredData.slice(0, 15).map(value => {
-            return (
-              <div className="dataItem">
-                <p>{value.text} </p>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">fridge-boi</Navbar.Brand>
+        
+        <Form>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+      </Container>
+    </Navbar>
   );
 }
+
+        {/* <div className='searchInput'>
+          <input type="text" placeholder="Enter name of the food you want ..." />
+          <div className="searchIcon">
+            {filteredData.length === 0 ? (
+              <BsSearch />
+            ) : (
+              <AiOutlineCloseCircle id="clearBtn" onClick={clearInput} />
+            )}
+          </div>
+        </div>
+        {filteredData.length != 0 && (
+          <div className="dataResult">
+            {filteredData.slice(0, 15).map(value => {
+              return (
+                <div className="dataItem">
+                  <p>{value.text} </p>
+                </div>
+              );
+            })}
+          </div>
+        )} */}
 
 export default SearchBar;
